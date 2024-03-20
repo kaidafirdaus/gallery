1300c7f9b17b4a4b35b025c6baf0f402a24b1daf
unknown <riskanuranggraeni172@gmail.com>
2024-03-20 12:18:01 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
                      <div class="d-flex justify-content-between">
                         <div>Images</div>
                      <div> 
@@ -30,34 +30,152 @@
                     <div class="row">
                         <div class="col-md-3">
                            <div class="list-group">
-                           <a href="#" class="list-group-item list-group-item-action">First item</a>
-                           <a href="#" class="list-group-item list-group-item-action">Second item</a>
-                           <a href="#" class="list-group-item list-group-item-action">Third item</a>
+                           <a href="#" class="list-group-item list-group-item-action">Personal</a>
+                           <a href="#" class="list-group-item list-group-item-action">Friends</a>
+                           <a href="#" class="list-group-item list-group-item-action">Family</a>
                         </div>
                     </div>
-                    <div class="col-md-9">
-                         <button data-toggle="collapse" class="btn-success" data-target="#demo">Add Image</button>
+                    <form class="col-md-9">
 
-                         <div id="demo" class="collapse">
-                         
-                            <form action="/action_page.php">
-                            <div class="form-group">
-                               <label for="email">Image Caption</label>
-                               <input type="text" class="form-control" placeholder="Enter email" id="email">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button data-toggle="collapse" class="btn btn-success" data-target="#demo">Add Image</button>
                             </div>
-                            <div class="form-group">
-                                <label for="pwd">Password:</label>
-                                <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+                            <div class="col-md-12">
+                                <div id="demo" class="collapse">
+                                
+                                   <form action="/action_page.php" id="image_upload_form">
+                                   <div class="form-group">
+                                      <label for="Caption">Image Caption</label>
+                                      <input type="text" name="caption" class="form-control" placeholder="Enter Caption" id="email">
+                                   </div>
+                                   <div class="form-group">
+                                       <div class="form-group">
+                                           <label for="sel1">Select Category</label>
+                                           <select name="category" class="form-control" id="category ">
+                                             <option value="">Select a category</option>
+                                             <option value="personal">Personal</option>
+                                             <option value="friends">Friends</option>
+                                             <option value="family">Family</option>
+                                           </select>
+                                         </div>              
+                                         <div class="form-group">
+                                           <label class="control-label">Upload Image</label>
+                                           <div class="preview-zone hidden">
+                                             <div class="box box-solid">
+                                               <div class="box-header with-border">
+                                                 <div><b>Preview</b></div>
+                                                 <div class="box-tools pull-right">
+                                                   <button type="button" class="btn btn-danger btn-xs remove-preview">
+                                                     <i class="fa fa-times"></i> Reset This Form
+                                                   </button>
+                                                 </div>
+                                               </div>
+                                               <div class="box-body"></div>
+                                             </div>
+                                           </div>
+                                           <div class="dropzone-wrapper">
+                                             <div class="dropzone-desc">
+                                               <i class="glyphicon glyphicon-download-alt"></i>
+                                               <p>Choose an image file or drag it here.</p>
+                                             </div>
+                                             <input type="file" name="image" class="dropzone">
+                                           </div>
+                                         </div>
+                                       
+                                   </div>
+                                   
+                                       <button type="submit" class="btn btn-primary">Submit</button>
+                                     </form>
+       
+                                      </div>
+                                 </div>
+
                             </div>
-                            <div class="form-group form-check">
-                              <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox"> Remember me
-                              </label>
-                            </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                              </form>
-                         </div>
+
                    </div>
-                  </div>
-        </div>
+              </div>
+              <div class="col-md-12 nt-4">
+                <div class="row">
+                    <div class="col-md-3 mb-4">
+                        <a href="#">
+                            <img src="https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.com" height="100%" width="100%">
+                        </a>
+                    </div>
+                    <div class="col-md-3 mb-4">
+                        <a href="#">
+                            <img src="https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.com" height="100%" width="100%">
+                        </a>
+                    </div>
+                    <div class="col-md-3 mb-4">
+                        <a href="#">
+                            <img src="https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.com" height="100%" width="100%">
+                        </a>
+                    </div>
+                    <div class="col-md-3 mb-4">
+                        <a href="#">
+                            <img src="https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.com" height="100%" width="100%">
+                        </a>
+                    </div>
+    </div>
+</div> 
+@endsection
+
+@section('js')
+<script type="text/javascript">
+    function readFile(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+        var htmlPreview =
+          '<img width="200" src="' + e.target.result + '" />' +
+          '<p>' + input.files[0].name + '</p>';
+        var wrapperZone = $(input).parent();
+        var previewZone = $(input).parent().parent().find('.preview-zone');
+        var boxZone = $(input).parent().parent().find('.preview-zone').find('.box').find('.box-body');
+  
+        wrapperZone.removeClass('dragover');
+        previewZone.removeClass('hidden');
+        boxZone.empty();
+        boxZone.append(htmlPreview);
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  
+  function reset(e) {
+    e.wrap('<form>').closest('form').get(0).reset();
+    e.unwrap();
+  }
+  
+  $(".dropzone").change(function() {
+    readFile(this);
+  });
+  
+  $('.dropzone-wrapper').on('dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).addClass('dragover');
+  });
+  
+  $('.dropzone-wrapper').on('dragleave', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).removeClass('dragover');
+  });
+  
+  $('.remove-preview').on('click', function() {
+    var boxZone = $(this).parents('.preview-zone').find('.box-body');
+    var previewZone = $(this).parents('.preview-zone');
+    var dropzone = $(this).parents('.form-group').find('.dropzone');
+    boxZone.empty();
+    previewZone.addClass('hidden');
+    reset(dropzone);
+  });
+  
+
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -16,7 +18,10 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/additional-methods.min.js"></script>
+    <!-- Styles -->
+    <link href="{{ asset('custom.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -78,5 +83,7 @@
             @yield('content')
         </main>
     </div>
+
+    @yield('js')
 </body>
 </html>
